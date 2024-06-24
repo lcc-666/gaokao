7d987f9970ec5d718b6a4bc0d71d1e64a12bab41
lcc-666 <2502247952@qq.com>
2024-06-24 22:56:05 +0800

    修改mongo验证方式

--- fun/pymon.py
+++ fun/pymon.py
@@ -2,5 +2,5 @@ def conn_mongo():
     from pymongo import MongoClient
     host = 'mongo'
     client = MongoClient(host=host, port=27017, username="admin", password="123456", authSource='admin',
-                         authMechanism='SCRAM-SHA-1')
+                         authMechanism='SCRAM-SHA-256')
     return client
